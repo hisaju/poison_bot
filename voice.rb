7d7ad3a21109fee027f9b69e7d2d31fc1ba6f341
hisaju <hisaju@gmail.com>
2023-08-02 21:11:08 +0900

    Discord Bot

---
+++ voice.rb
@@ -0,0 +1,17 @@
+require 'dotenv/load'
+require 'discordrb'
+
+bot = Discordrb::Bot.new token: ENV.fetch('DISCORDRB_TOKEN')
+
+bot.message do |event|
+  if event.content == '!poison'
+    event.message.embeds.each do |embed|
+      bot.voice_connect(embed.title)
+      bot.voice(event.server).play_file(embed.url)
+    end
+    event.message.delete
+    bot.voice(event.server).destroy
+  end
+end
+
+bot.run
